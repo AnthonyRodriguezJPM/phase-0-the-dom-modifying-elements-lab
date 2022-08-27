457385e39173837568804b26ececb0d1d12499f8
Anthony Rodriguez <rdz.tony1@gmail.com>
2022-08-27 02:25:48 -0500

    commit tree facaf621f4504ac70d1c85f46e7313b9e62b7aae

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code here!
+
+const removeMain=document.querySelector('#main');
+    removeMain.remove();
+
+const newHeader = document.createElement('h1');
+   //document.body.append(newHeader);
+    newHeader.id = 'victory';
+    newHeader.innerHTML = "Anthony is the champion"
+    console.log(newHeader)
+
+    
+
+    
+
+   
+
+
